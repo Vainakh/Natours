5d53ed8ba9f788b87443b1820f9da8ef10939528
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-02-19 08:16:02 -0800

    File structure intro

---
+++ controllers/userController.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+
+const users = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/users.json`));
+
+getAllUsers = (req, res) => {
+  res
+  .status(500)
+  .json({
+    status: 'error',
+    message: "This route is not yet defined"
+  })
+}
+
+getUser = (req, res) => {
+  res
+  .status(500)
+  .json({
+    status: 'error',
+    message: "This route is not yet defined"
+  })
+}
+
+createUser = (req, res) => {
+  res
+  .status(500)
+  .json({
+    status: 'error',
+    message: "This route is not yet defined"
+  })
+}
+
+updateUser = (req, res) => {
+  res
+  .status(500)
+  .json({
+    status: 'error',
+    message: "This route is not yet defined"
+  })
+}
+
+deleteUser = (req, res) => {
+  res
+  .status(500)
+  .json({
+    status: 'error',
+    message: "This route is not yet defined"
+  })
+}
+
+module.exports = {getAllUsers, getUser, createUser, updateUser, deleteUser};
