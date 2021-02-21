38dc583f5541afebb6b9fb252ad11c18502565ce
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-02-21 08:29:14 -0800

    Refacture for MVC

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
@@ -19,38 +19,6 @@ mongoose
   .then(() => {
     console.log('DB connection is success!');
   });
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name!'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: String,
-    required: [true, 'A tour must have a price!'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997,
-});
-
-testTour
-.save()
-.then(doc => {
-  console.log(doc);
-})
-.catch(err => {
-  console.log('ERROR:', err);
-})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
