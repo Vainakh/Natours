c64ef3ed1a582f192986842f7ebfb667bbce189d
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-02-22 10:22:11 -0800

    Delete entry

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -84,11 +84,19 @@ updateTour = async (req, res) => {
   }
 };
 
-deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 module.exports = {
