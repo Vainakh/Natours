5aaf8453e24c3640fa21f73011913c5723ca60b5
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-02-22 07:09:33 -0800

    Try alternative way of create doc in database

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,4 @@
-const Tour = require("../models/tourModel");
+const Tour = require('../models/tourModel');
 
 // const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
@@ -6,23 +6,21 @@ checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
       status: 'fail',
-      message: 'Missing name or price'
+      message: 'Missing name or price',
     });
   }
   next();
-}
+};
 
 getAllTours = (req, res) => {
-  res
-  .status(200)
-  .json({
+  res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
     // results: tours.length,
     // data: {
     //   tours: tours
     // }
-  })
+  });
 };
 
 getTour = (req, res) => {
@@ -34,29 +32,44 @@ getTour = (req, res) => {
   //     tour: tour
   //   }
   // });
-}
+};
 
-createTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // }
-  })
-}
+createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+    });
+  }
+};
 
 updateTour = (req, res) => {
   res.status(200).json({
-    status: "fail",
-    message: "Invalid ID"
-  })
-}
+    status: 'fail',
+    message: 'Invalid ID',
+  });
+};
 
-deleteTour = (req, res) => { 
+deleteTour = (req, res) => {
   res.status(204).json({
-    status: "success",
-    data: null
-  })
-}
+    status: 'success',
+    data: null,
+  });
+};
 
-module.exports = {getAllTours, getTour, createTour, updateTour, deleteTour, checkBody};
+module.exports = {
+  getAllTours,
+  getTour,
+  createTour,
+  updateTour,
+  deleteTour,
+  checkBody,
+};
