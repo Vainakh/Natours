cec9448e63f8459ba412857b5664a21b5554c53a
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-02-25 04:47:21 -0800

    Aggregation pipeline, matching and grouping

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -102,6 +102,41 @@ deleteTour = async (req, res) => {
   }
 };
 
+getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      { $sort: { avgPrice: 1 } },
+      { $match: { _id: { $ne: 'EASY' } } },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: stats,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
 module.exports = {
   aliasTopTours,
   getAllTours,
@@ -109,6 +144,7 @@ module.exports = {
   createTour,
   updateTour,
   deleteTour,
+  getTourStats,
 };
 
 //  checkBody,
