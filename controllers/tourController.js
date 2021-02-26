6d0124af0683a4a517e1df95aa7386d478d5336f
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-02-26 07:37:24 -0800

    Data validation: built-in validators

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -72,6 +72,7 @@ updateTour = async (req, res) => {
   try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
+      runValidators: true,
     });
     res.status(200).json({
       status: 'success',
