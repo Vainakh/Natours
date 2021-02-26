12b79ab9639e1285b0765dd3266d9d70eb18e6b9
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-02-26 06:29:26 -0800

    Query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -56,6 +56,10 @@ const tourSchema = new mongoose.Schema({
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -73,17 +77,32 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-tourSchema.pre('save', function (next) {
-  console.log('Will save document...')
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save document...')
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+
+//query middleware
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
   next();
 });
 
-tourSchema.post('save', function (doc, next) {
-  console.log(doc);
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`)
+  console.log(docs);
   next();
 });
 
 tourSchema.plugin(require('mongoose-beautiful-unique-validation'));
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
